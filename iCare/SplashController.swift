e6e12470b482407040735ad5010e0136ca924975
lelea2 <daomaikhanh1988@yahoo.com>
2016-11-27 09:35:55 -0800

    Initial check in

---
+++ iCare/SplashController.swift
@@ -0,0 +1,61 @@
+//
+//  SplashController.swift
+//  iCare
+//
+//  Created by Khanh Dao on 11/27/16.
+//  Copyright © 2016 Khanh Dao. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SplashController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        addBackgroundImage()
+        addLogo()
+        
+        // Show the home screen after a bit. Calls the show() function.
+        _ = Timer.scheduledTimer(
+            timeInterval: 2.5, target: self, selector: #selector(UIAlertController.show), userInfo: nil, repeats: false
+        )
+    }
+    
+    /*
+     * Shows the app's main home screen.
+     * Gets called by the timer in viewDidLoad()
+     */
+    func show() {
+        self.performSegue(withIdentifier: "showApp", sender: self)
+    }
+    
+    /*
+     * Adds background image to the splash screen
+     */
+    func addBackgroundImage() {
+        let screenSize: CGRect = UIScreen.main.bounds
+        let bg = UIImage(named: "bg_img.jpg")
+        let bgView = UIImageView(image: bg)
+        
+        bgView.frame = CGRect(x: 0, y: 0, width: screenSize.width, height: screenSize.height)
+        self.view.addSubview(bgView)
+    }
+    
+    /*
+     * Adds logo to splash screen
+     */
+    func addLogo() {
+        let screenSize: CGRect = UIScreen.main.bounds
+        
+        let logo     = UIImage(named: "logo.png")
+        let logoView = UIImageView(image: logo)
+        
+        let w = logo?.size.width
+        let h = logo?.size.height
+        
+        logoView.frame = CGRect( x: (screenSize.width/2) - (w!/2), y: 5, width: w!, height: h! )
+        self.view.addSubview(logoView)
+    }
+}
---
+++ iCare/iCare/ProfileController.swift
@@ -0,0 +1,25 @@
+//
+//  ProfileController.swift
+//  iCare
+//
+//  Created by Khanh Dao on 11/27/16.
+//  Copyright © 2016 Khanh Dao. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ProfileController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
